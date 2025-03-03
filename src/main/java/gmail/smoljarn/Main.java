68d9effa2a19777ae294daee06da2da5da4ca22e
26Ninylichka <gmail.smoljarn@>
2025-03-03 23:49:59 +0100

    add . homework1

--- src/main/java/gmail/smoljarn/Main.java
+++ src/main/java/gmail/smoljarn/Main.java
@@ -2,6 +2,11 @@ package gmail.smoljarn;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] array = {100, 23, 45, 42, 12, 19};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.println(array[i] + " ");
+            }
+        }
     }
 }
