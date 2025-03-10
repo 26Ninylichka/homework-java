3af4f8df152faa0d489b62c452af6979efb2e495
26Ninylichka <gmail.smoljarn@>
2025-03-10 19:22:40 +0100

    add . lesson14 add. voice

--- src/main/java/gmail/smoljarn/lesson14/animals/Dragon.java
+++ src/main/java/gmail/smoljarn/lesson14/animals/Dragon.java
@@ -5,17 +5,29 @@ public  class Dragon {
     private String name;
     private int age;
     private double weight;
+    private String voice;
 
+
+    @Override
+    public String toString() {
+        return "Dragon{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                ", voice='" + voice + '\'' +
+                '}';
+    }
 
     public Dragon() {
         System.out.println("Dragon Constructor called");
     }
 
-    public Dragon(String name, int age, double weight){
+    public Dragon(String name, int age, double weight, String voice){
         System.out.println("Dragon Constructor WITH PARAMETERS called");
         this.name = name;
         this.age = age;
         this.weight = weight;
+        this.voice = voice;
     }
 
 
@@ -46,6 +58,12 @@ public  class Dragon {
     }
 
 
+    public void setVoice(String voice) {
+        this.voice = voice;
     }
+    public String getVoice() {
+        return voice;
+    }
+}
 
 
